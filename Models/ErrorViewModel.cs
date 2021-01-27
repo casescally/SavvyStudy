4ee743cd84278ab4d5d3b18747e37715e5d277d3
Case Scally <caseScally@gmail.com>
2021-01-27 12:59:13 -0600

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SavvyStudy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Word.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SavvyStudy.Models
+{
+    public class Word
+    {
+        public int Id { get; set; }
+        public string Untranslated { get; set; }
+        public string Translated { get; set; }
+        public string Pronunciation { get; set; }
+        public int Phrase { get; set; }
+        public string Language { get; set; }
+    }
+}
